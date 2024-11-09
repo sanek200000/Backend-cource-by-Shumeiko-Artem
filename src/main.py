8c6b7207ef14933a1b9b3d42bda821ef99a86db6
heist <heist2000@gmail.com>
2024-11-09 19:49:49 +0300

    add get and delete endpoints

--- src/main.py
+++ src/main.py
@@ -1,11 +1,32 @@
-from fastapi import FastAPI
+from typing import Any
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
+hotels = [
+    {"id": 1, "title": "Sochi"},
+    {"id": 2, "title": "Dubai"},
+]
+
+
+@app.get("/hotels")
+def get_hotels(
+    id: int,
+    title: str = Query(description="Название отеля"),
+) -> list[dict[str, Any]]:
+    return [h for h in hotels if h["title"] == title and h["id"] == id]
+
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotel(hotel_id: int) -> dict[str, str]:
+    global hotels
+    hotels = [h for h in hotels if h["id"] != hotel_id]
+    return {"status": "OK"}
+
 
 @app.get("/")
-def func() -> str:
-    return "Hello World!!!!!!!!!!"
+def func() -> list[dict[str, Any]]:
+    return hotels
 
 
 if __name__ == "__main__":
