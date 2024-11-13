59363243bcc3790b4159876d30f143857158ea2e
heist <heist2000@gmail.com>
2024-11-13 13:54:23 +0300

    make migration

---
+++ src/migrations/versions/30bc6b6d68ae_initial_migration.py
@@ -0,0 +1,33 @@
+"""initial migration
+
+Revision ID: 30bc6b6d68ae
+Revises: 
+Create Date: 2024-11-13 10:47:18.058946
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "30bc6b6d68ae"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "hotels",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.Column("location", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("hotels")
