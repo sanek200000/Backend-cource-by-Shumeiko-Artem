1e29f52a52b8a465c95a5a8555144c840c81494f
heist <heist2000@gmail.com>
2024-11-13 13:53:57 +0300

    add url to config for alembic

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -5,12 +5,14 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from conf import settings
 from db import Base
 from models.hotels import HotelsOrm
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", f"{settings.DB_URL}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
