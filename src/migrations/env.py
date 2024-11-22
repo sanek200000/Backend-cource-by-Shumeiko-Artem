2986cc462d1cde8699e5e4e7fe0daf43f1acbbcb
heist <heist2000@gmail.com>
2024-11-23 00:11:17 +0300

    add facilities model

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -8,9 +8,11 @@ from alembic import context
 from conf import settings
 from db import Base
 from models.bookings import BookingsOrm
+from models.facilities import FacilitiesOrm
 from models.hotels import HotelsOrm
 from models.rooms import RoomsOrm
 from models.users import UsersOrm
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ src/models/facilities.py
@@ -0,0 +1,19 @@
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from db import Base
+
+
+class FacilitiesOrm(Base):
+    __tablename__ = "facilities"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(length=100))
+
+
+class RoobsFacilitiesOrm(Base):
+    __tablename__ = "rooms_facilities"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
+    facility_id: Mapped[int] = mapped_column(ForeignKey("facilities.id"))
