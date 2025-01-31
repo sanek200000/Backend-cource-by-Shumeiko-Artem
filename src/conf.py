252664ee09aa50e969920b19f990784789d978fd
heist <heist2000@gmail.com>
2025-01-31 19:12:01 +0300

    add testing and development DevContainers

--- src/conf.py
+++ src/conf.py
@@ -3,7 +3,8 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    MODE: Literal["TEST", "LOCAL", "DEV", "PROD"]
+    # MODE: Literal["TEST", "LOCAL", "DEV", "PROD"]
+    MODE: str
 
     DB_HOST: str
     DB_NAME: str
