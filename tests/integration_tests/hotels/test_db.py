86fd6a185f286706c1893b331855e2e6ecf83c40
heist <heist2000@gmail.com>
2025-02-01 15:53:52 +0300

    add test_db

---
+++ tests/integration_tests/hotels/test_db.py
@@ -0,0 +1,11 @@
+from db import ASYNC_SESSION_MAKER
+from schemas.hotels import HotelAdd
+from utils.db_manager import DBManager
+
+
+async def test_add_hotel():
+    hotel_data = HotelAdd(title="Hotel 5 stars", location="Sochi")
+
+    async with DBManager(session_factory=ASYNC_SESSION_MAKER) as db:
+        new_hotel_data = await db.hotels.add(hotel_data)
+        print(f"{new_hotel_data}")
