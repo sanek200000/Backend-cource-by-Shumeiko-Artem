b2c56a2cc8959949fa2ae6f69929ad269eda6fad
heist <heist2000@gmail.com>
2025-02-06 16:48:01 +0300

    solution task #21

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -1,10 +1,19 @@
-from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
 
 
-async def test_get_hotels(ac):
-
+async def test_get_ficilities(ac):
     response = await ac.get("/facilities")
-    print(f"=================== {response.json() = } ===========================")
 
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+async def test_post_ficilities(ac):
+    facility_title = "Массаж"
+    response = await ac.post("/facilities", json={"title": facility_title})
+
+    res = response.json()
+    assert response.status_code == 200
+    assert isinstance(res, dict)
+    assert "data" in res
+    assert res.get("data").get("title") == facility_title
